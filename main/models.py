a32fd6384f78eacdb5295b0f0a5c152932c4c2da
Jnorman2123 <54901101+Jnorman2123@users.noreply.github.com>
2020-07-02 18:52:36 -0400

    created user and main class in models and migrated them to the database

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Availability(models.Model):
+    date_available = models.DateField(default='')
+    start_time = models.TimeField(default='')
+    end_time = models.TimeField(default='')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # date_created = models.DateTimeField
