b6efb21ab569580fdf92e67425b6980720519510
Jnorman2123 <54901101+Jnorman2123@users.noreply.github.com>
2020-07-02 19:50:31 -0400

    added user and availability classes to admin and modified their display

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User, Availability
 
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class AvailabilityAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date_available', 'start_time', 'end_time')
+    exclude = ('date_created', )
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Availability, AvailabilityAdmin)
--- main/models.py
+++ main/models.py
@@ -7,6 +7,9 @@ from django.utils import timezone
 class User(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Availability(models.Model):
     date_available = models.DateField(default='')
