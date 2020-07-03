8fbff44e235276ec9ea10ef45b208f4c2009d64f
Jnorman2123 <54901101+Jnorman2123@users.noreply.github.com>
2020-07-02 20:09:02 -0400

    added index template to main changed main index view to render index template

--- main/views.py
+++ main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Availability
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse("Hello world")
+    availabilities = Availability.objects.all()
+    return render(request, 'index.html', {'availabilities': availabilities})
